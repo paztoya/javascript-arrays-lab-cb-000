3f2e34004782844482e8d7abcfa3d41edd2d03cf
Daniel Paz <daniel@paztoya.com>
2020-01-21 00:22:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  return kittens.splice(0, kittens.length -1)
+  return kittens.slice(0, kittens.length -1)
 }
 
 function removeFirstKitten(){
